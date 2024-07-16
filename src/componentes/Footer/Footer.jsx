da7f44ce43438d6eb60766d83811ae3c4158375d
Marko <160692804+Milkshades@users.noreply.github.com>
2024-07-16 06:32:09 -0500

    Add files via upload

---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import logo from './Logo.png';
+import './Footer.css'
+
+const Footer = () => {
+    return < div className="pie">
+        <a className='logo__pie'><img src={logo} /></a>
+        <h5>Creado por MARKO PEREZ | ONE G6 | 2024</h5>
+    </ div>
+}
+
+export default Footer;
